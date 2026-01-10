7767429c799e91e5ef4c20170babd2a3ef750def
sor999 <sorsor999@naver.com>
2026-01-10 10:45:50 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/test/java/swyp12/team9/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package swyp12.team9.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
