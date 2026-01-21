9447f00d2a0b6ac560d2a4627db01139d9382f99
sor999 <sorsor999@naver.com>
2026-01-21 23:26:48 +0900

    feat: 엔티티 추가

---
+++ src/main/java/swyp12/team9/server/domain/link/model/Link.java
@@ -0,0 +1,41 @@
+package swyp12.team9.server.domain.link.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import swyp12.team9.server.global.common.entity.BaseEntity;
+
+@Entity
+@Table(name = "links")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Link extends BaseEntity {
+
+    @Id
+    @Column(name = "link_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "url", nullable = false, length = 2048)
+    private String url;
+
+    @Column(name = "title", length = 300)
+    private String title;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @Column(name = "preview_image_url", length = 1024)
+    private String previewImageUrl;
+
+    @Column(name = "ai_summary", columnDefinition = "TEXT")
+    private String aiSummary;
+
+    @Builder
+    public Link(String url, String title, String description, String previewImageUrl) {
+        this.url = url;
+        this.title = title;
+        this.description = description;
+        this.previewImageUrl = previewImageUrl;
+    }
+}
