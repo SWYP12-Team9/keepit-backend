65e1d1ce3a6b8cf770ce73d54527a60f4e7a0a27
sor999 <sorsor999@naver.com>
2026-01-31 23:05:48 +0900

    fix: SwaggerConfig 설정 변경

--- src/main/java/swyp12/team9/server/global/config/SwaggerConfig.java
+++ src/main/java/swyp12/team9/server/global/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package swyp12.team9.server.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +19,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         String securitySchemeName = "AccessToken";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
 
         return new OpenAPI()
                 .info(new Info()
@@ -29,6 +31,7 @@ public class SwaggerConfig {
                                 .url(serverUrl)
                                 .description("Server")
                 ))
+                .addSecurityItem(securityRequirement)
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
                                 new SecurityScheme()
