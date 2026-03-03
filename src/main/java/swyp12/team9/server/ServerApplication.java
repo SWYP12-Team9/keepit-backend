1ffa1d45215d2fd19ece386711c57cf997bf0d64
yjinmo9 <152494912+yjinmo9@users.noreply.github.com>
2026-03-03 16:36:16 +0900

    chore: 로그백 설정 파일 분리 및 환경별 로깅 정책 적용

--- src/main/java/swyp12/team9/server/ServerApplication.java
+++ src/main/java/swyp12/team9/server/ServerApplication.java
@@ -2,10 +2,8 @@ package swyp12.team9.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
 public class ServerApplication {
