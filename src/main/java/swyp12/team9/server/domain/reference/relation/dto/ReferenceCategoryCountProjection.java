6550b20821ef688a861db315a9e83a537d6a92ec
whitejh <jhk0806@gmail.com>
2026-03-05 15:56:11 +0900

    refactor:  /jwt/exchange, /jwt/refresh API를 AuthController로 이동

---
+++ src/main/java/swyp12/team9/server/domain/reference/relation/dto/ReferenceCategoryCountProjection.java
@@ -0,0 +1,20 @@
+package swyp12.team9.server.domain.reference.relation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 레퍼런스 카테고리별 개수 Projection
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReferenceCategoryCountProjection {
+    private Long referenceId;
+    private String referenceTitle;
+    private String referenceColorCode;
+    private Long count;
+}
---
+++ src/main/java/swyp12/team9/server/domain/reference/relation/dto/ReferenceWithCountProjection.java
@@ -0,0 +1,11 @@
+package swyp12.team9.server.domain.reference.relation.dto;
+
+/**
+ * 레퍼런스 ID, 이름, 링크 개수, 색상 코드 Projection
+ */
+public interface ReferenceWithCountProjection {
+    Long getReferenceId();
+    String getReferenceTitle();
+    Long getCount();
+    String getColorCode();
+}
